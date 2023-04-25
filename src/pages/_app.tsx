d5f38a1e3cec0588e60eb6a35d0db24228f5f85b
RielJ <bulaybulay.rielj@gmail.com>
2023-04-25 12:51:52 +0800

    Added fonts and swiper css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,42 @@
 import '@/styles/globals.css'
 import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { JetBrains_Mono, Judson, Kavoon } from 'next/font/google'
+
+import 'swiper/css'
+import 'swiper/css/autoplay'
+
+const jetBrains = JetBrains_Mono({
+  subsets: ['latin'],
+  variable: '--font-jetbrains-mono',
+})
+
+const judson = Judson({
+  weight: '700',
+  subsets: ['latin'],
+  variable: '--font-judson',
+})
+
+const kavoon = Kavoon({
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-kavoon',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>RielJ Portfolio</title>
+      </Head>
+      <ThemeProvider attribute="class">
+        <main
+          className={`${jetBrains.variable} ${judson.variable} ${kavoon.variable} font-primary`}
+        >
+          <Component {...pageProps} />
+        </main>
+      </ThemeProvider>
+    </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,27 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '1.5rem',
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+      },
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        primary: ['var(--font-jetbrains-mono)'],
+        secondary: ['var(--font-judson)'],
+        logo: ['var(--font-kavoon)'],
       },
     },
   },
