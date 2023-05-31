1595f8dff7baa6cf5deacc8f8e6b25fe29ac79a6
RielJ <bulaybulay.rielj@gmail.com>
2023-05-31 10:31:22 +0800

    refactor: upgrade next packages and added three related packages

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const path = require('path')
 
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
