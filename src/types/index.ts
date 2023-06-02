7a671afe39787a04edf35a6b2885be0a6d7307e3
RielJ <bulaybulay.rielj@gmail.com>
2023-06-02 14:10:58 +0800

    refactor: types into one file

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,13 @@
-export * from './portfolio'
+export interface IPortfolioCard {
+  name: string
+  description: string
+  image?: string
+}
+
+export interface IExperienceData {
+  dateStart: string
+  dateEnd: string
+  company: string
+  role: string
+  description: string[]
+}
