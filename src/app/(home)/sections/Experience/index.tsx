067cbdfa8e028982f6821a4db16bd30c448e79f2
RielJ <bulaybulay.rielj@gmail.com>
2023-06-02 14:11:46 +0800

    feat: experience timeline

--- src/app/(home)/sections/Experience/index.tsx
+++ src/app/(home)/sections/Experience/index.tsx
@@ -1,7 +1,161 @@
-import React from 'react'
+'use client'
+import { experiences } from '@/constants'
+import clsx from 'clsx'
+import React, { useRef } from 'react'
+import styles from './Experience.module.scss'
+import { useVisibleScroll, useWindowSize } from '@/hooks'
+import { motion } from 'framer-motion'
+import { IExperienceData } from '@/types'
+
+const Line = () => {
+  const ref = useRef<HTMLDivElement>(null)
+  const scroll = useVisibleScroll(ref)
+  const screenSize = useWindowSize()
+  const lineHeight =
+    scroll &&
+    screenSize &&
+    Math.max(
+      0,
+      Math.min(
+        scroll.y + screenSize.height / 2 - scroll.offsetBoundingRect.top,
+        ref.current?.clientHeight ?? 0
+      )
+    )
+
+  return (
+    <div ref={ref} className="w-full h-full absolute">
+      <div
+        className={clsx(styles['timeline-lottie'])}
+        style={{
+          height: lineHeight,
+        }}
+      />
+    </div>
+  )
+}
+
+const Circle = ({ index }: { index: number }) => {
+  const ref = useRef<HTMLDivElement>(null)
+  const scroll = useVisibleScroll(ref)
+  const screenSize = useWindowSize()
+  const fill =
+    scroll &&
+    screenSize &&
+    scroll.y + screenSize.height / 2 > scroll.offsetBoundingRect.top - 30
+
+  return (
+    <div
+      ref={ref}
+      className={clsx(
+        'absolute w-full transform transition-colors duration-500'
+      )}
+    >
+      <div
+        className={clsx(
+          styles['timeline-lottie-circle'],
+          fill ? 'bg-[#915eff] scale-[2]' : 'bg-[#808080]',
+          index % 2
+            ? '-translate-x-1/2 left-[-60px]'
+            : 'translate-x-1/2 right-0',
+          '-translate-y-1/2 top-[-30px] transform transition-transform duration-300 ease-in-bounce'
+        )}
+      />
+    </div>
+  )
+}
+
+const Content = ({
+  experience,
+  index,
+}: {
+  experience: IExperienceData
+  index: number
+}) => {
+  // const ref = useRef<HTMLDivElement>(null)
+  // const scroll = useVisibleScroll(ref)
+  // const screenSize = useWindowSize()
+  // const contentTouched =
+  //   scroll &&
+  //   screenSize &&
+  //   scroll.y + screenSize.height / 2 > scroll.offsetBoundingRect.top - 30
+  const isRight = index % 2
+
+  const cardVariants = {
+    offscreen: {
+      opacity: 0,
+      // left: isRight ? 0 : 50,
+      right: 50,
+    },
+    onscreen: {
+      opacity: 1,
+      left: 0,
+      right: 0,
+      transition: {
+        type: 'spring',
+        bounce: 0.4,
+        duration: 0.8,
+      },
+    },
+  }
+
+  return (
+    <motion.div
+      // ref={ref}
+      className={clsx(styles['timeline-content'], 'relative space-y-3')}
+      variants={cardVariants}
+      initial="offscreen"
+      whileInView="onscreen"
+      viewport={{ once: true, amount: 0.8 }}
+    >
+      <h3
+        className={clsx(
+          isRight ? 'left-[-10px]' : 'right-[-10px]',
+          'top-[-55px] absolute'
+        )}
+      >
+        {experience.dateStart} - {experience.dateEnd}
+      </h3>
+      <h1>
+        {experience.role}{' '}
+        <span className="text-accent">@ {experience.company}</span>{' '}
+      </h1>
+      <ul className="list-disc">
+        {experience.description.map((desc) => (
+          <li key={desc} className="font-normal text-sm font-secondary">
+            <p>{desc}</p>
+          </li>
+        ))}
+      </ul>
+    </motion.div>
+  )
+}
 
 const Experience = () => {
-  return <section className="min-h-screen">Experience</section>
+  const timelineRef = useRef<HTMLDivElement | null>(null)
+
+  return (
+    <section className="relative w-full mx-auto min-h-screen text-center max-w-7xl  flex flex-col items-center justify-center gap-[3rem] snap-center">
+      <div className="flex-1 overflow-hidden w-full">
+        <h1 className="heading">Experience Timeline</h1>
+      </div>
+      <div ref={timelineRef} className={clsx(styles['timeline'], '')}>
+        <Line />
+        <ul className={clsx(styles['timeline-list'])}>
+          {experiences.map((experience, index) => {
+            return (
+              <li
+                key={experience.company + index}
+                className={clsx(styles['timeline-item'], 'relative')}
+              >
+                <Circle index={index} />
+                <Content experience={experience} index={index} />
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </section>
+  )
 }
 
 export { Experience }
